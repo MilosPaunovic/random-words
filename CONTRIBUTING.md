a87e3150d8095abac322fa54fb959ee8f634a364
Milos Paunovic <paun992@hotmail.com>
2022-04-25 15:54:32 +0200

    chore: Uniforming code style throughout the package

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -8,6 +8,6 @@ Please note we have a code of conduct, please follow it in all your interactions
 ## Pull Request Process
 
 1. Update the README.md with details of changes to the interface, this includes new environment 
-   variables, exposed ports, useful file locations and container parameters.
+	variables, exposed ports, useful file locations and container parameters.
 2. Increase the version numbers in any examples files and the README.md to the new version that this
-   Pull Request would represent. The versioning scheme we use is [SemVer](http://semver.org/).
+	Pull Request would represent. The versioning scheme we use is [SemVer](http://semver.org/).
--- test/lib.js
+++ test/lib.js
@@ -5,23 +5,23 @@ const RANDOM = initialize({ countryCode: 'rs' })
 
 describe('Testing basic functionalities of the entire library', () => {
 
-  it('word()  - Should return a random word', () => {
-    const result = RANDOM.word();
-    assert.isString(result) && assert.isNotEmpty(result);
-  });
+	it('word()  - Should return a random word', () => {
+		const result = RANDOM.word();
+		assert.isString(result) && assert.isNotEmpty(result);
+	});
 
-  it('words() - Should return array of 10 random words (no parameter passed)', () => {
-    const result = RANDOM.words();
-    assert.isArray(result) && assert.lengthOf(result, 10);
-  });
+	it('words() - Should return array of 10 random words (no parameter passed)', () => {
+		const result = RANDOM.words();
+		assert.isArray(result) && assert.lengthOf(result, 10);
+	});
 
-  it('words() - Should return array of 34 random words (with parameter passed)', () => {
-    const result = RANDOM.words(34);
-    assert.isArray(result) && assert.lengthOf(result, 34);
-  });
+	it('words() - Should return array of 34 random words (with parameter passed)', () => {
+		const result = RANDOM.words(34);
+		assert.isArray(result) && assert.lengthOf(result, 34);
+	});
 
-  it('words() - Should throw an error (invalid parameter passed)', () => {
-    assert.throw(() => { RANDOM.words("invalid"); }, Error, 'Parameter howMany must be convertible to number.');
-  });
+	it('words() - Should throw an error (invalid parameter passed)', () => {
+		assert.throw(() => { RANDOM.words("invalid"); }, Error, 'Parameter howMany must be convertible to number.');
+	});
 
 });
