abd7fa580059af6a642246bb0a1d27ddba9f2b61
Milos Paunovic <paun992@hotmail.com>
2022-04-25 16:20:00 +0200

    Added copyright information (#5)
    * chore: Uniforming code style throughout the package
    
    * chore: Added copyright information to the top of the entry file
    
    *chore: Reverted indentation to spaces in .yml files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'import/prefer-default-export': 'off',
-    'import/extensions': 'off',
-    'linebreak-style': 'off',
-    'no-unused-vars': 'off',
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'airbnb-base',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'indent': 'tab',
+		'import/prefer-default-export': 'off',
+		'import/extensions': 'off',
+		'linebreak-style': 'off',
+		'no-unused-vars': 'off',
+	},
 };
